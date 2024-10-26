30e4e5def142a6f0255ff820c3d4a6a4869fd00c
Hugo Pradier <hpradier@icloud.com>
2024-10-26 12:10:54 +0200

    add footer

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,17 +1,19 @@
 import Header from '@/components/header';
+import Footer from '@/components/footer';
 import React from 'react';
 
 interface LayoutProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
-      <Header />
-      <main className="pt-16">{children}</main>
-    </div>
-  );
+	return (
+		<div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
+			<Header />
+			<main className="pt-16">{children}</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default Layout;
