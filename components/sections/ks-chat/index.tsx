24a459eda14b89f85b198c24d467f326f5d1a90d
noe campo <noecampo@delosintelligence.fr>
2024-12-23 13:55:48 +0100

    lonole

--- components/sections/ks-chat/index.tsx
+++ components/sections/ks-chat/index.tsx
@@ -934,7 +934,7 @@ Please provide suggestions on how generative AI could improve my business operat
         </svg>
       </button>
       <div
-        className={`relative overflow-auto md:overflow-hidden max-w-4xl mx-auto ${messages.length === 0 ? "h-screen" : "h-screen"}`}
+        className={`relative overflow-auto pt-20 md:pt-0 md:overflow-hidden max-w-4xl mx-auto ${messages.length === 0 ? "h-screen" : "h-screen"}`}
       >
         {messages.length === 0 ? (
           <motion.div
