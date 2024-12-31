6e68265378e73a0b30d55492c25ef1c62ca471f6
Hugo Pradier <hpradier@icloud.com>
2024-12-31 18:32:00 +0100

    oznfzenfzenfzeljkfnzejlnfzkjef rend zinzin

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,12 @@
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
 	images: {
 		domains: [
 			'api.microlink.io',
 			'images.unsplash.com',
 			'assets.aceternity.com',
 		],
-		formats: ['image/webp'],
-		deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-		minimumCacheTTL: 31536000,
-	},
-	headers: () => [
-		{
-			source: '/:path*',
-			headers: [
-				{
-					key: 'Content-Security-Policy',
-					value:
-						"default-src 'self'; img-src 'self' api.microlink.io images.unsplash.com assets.aceternity.com; style-src 'self' 'unsafe-inline';",
-				},
-				{
-					key: 'Cache-Control',
-					value: 'public, max-age=31536000, immutable',
-				},
-			],
-		},
-	],
-	experimental: {
-		optimizeCss: true,
 	},
 };
 
