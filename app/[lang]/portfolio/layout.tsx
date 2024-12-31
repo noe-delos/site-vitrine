b80a847511b197d59308dbd7b1a17709658a08f0
Hugo Pradier <hpradier@icloud.com>
2024-12-31 17:31:51 +0100

    delete potfolio page useless code

--- app/[lang]/portfolio/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main className="pt-16">{children}</main>;
-}
--- app/[lang]/portfolio/page.tsx
+++
@@ -1,8 +0,0 @@
-import PortfolioPage from '@/components/sections/portfolio';
-import { getDictionary } from '@/get-dictionary';
-
-export default async function Page({ params }: any) {
-  const dictionary = await getDictionary(params.lang);
-
-  return <PortfolioPage dictionary={dictionary} />;
-}
